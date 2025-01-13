d679f705ccea986b432cd925b5b8fcf67bd841e2
Иванов Владислав Денисович <158275412+IvanovVladislav03@users.noreply.github.com>
2025-01-13 21:19:01 +0400

    added UserRepository

---
+++ EncryptedMessenger/EncryptedMessenger.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,63 @@
+﻿using AutoMapper;
+using EncryptedMessenger.Domain.Interfaces;
+using EncryptedMessenger.Domain.Models;
+using EncryptedMessenger.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncryptedMessenger.Infrastructure.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly AppDbContext _context;
+        private readonly IMapper _mapper;
+
+        public UserRepository(AppDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+        public async Task AddUserAsync(User user)
+        {
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteUserAsync(Guid id)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Id == id);
+            if (user != null)
+                _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<User>> GetAllUsersAsync()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        public async Task<User> GetUserByIdAsync(Guid id)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u=> u.Id == id);
+            return _mapper.Map<User>(user);
+        }
+
+        public async Task<User> GetUserByUsernameAsync(string username)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Username == username);
+            return _mapper.Map<User>(user);
+        }
+
+        public async Task UpdateUserAsync(User user)
+        {
+            var existingUser = await GetUserByIdAsync(user.Id);
+            _context.Entry(existingUser).CurrentValues.SetValues(user);
+
+            await _context.SaveChangesAsync();
+        }
+    }
+}
