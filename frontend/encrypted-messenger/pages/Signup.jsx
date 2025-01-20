76b5fda05e35870d176d9a7699a36ba6ab43021c
Иванов Владислав Денисович <158275412+IvanovVladislav03@users.noreply.github.com>
2025-01-20 13:48:48 +0400

    added register page

--- frontend/encrypted-messenger/pages/Signup.jsx
+++ frontend/encrypted-messenger/pages/Signup.jsx
@@ -1,5 +1,5 @@
 import { FormControl, FormLabel } from "@chakra-ui/form-control";
-import { AddIcon, LockIcon, StarIcon } from "@chakra-ui/icons";
+import { AddIcon, ArrowDownIcon, LockIcon, StarIcon } from "@chakra-ui/icons";
 import { InputRightElement, InputGroup } from "@chakra-ui/input";
 import {
   Box,
@@ -16,13 +16,10 @@ const Signin = () => {
     <Box className="flex items-center justify-center min-h-screen">
       <Box className="shadow-lg bg-white p-4 rounded-md w-96">
         <Box className="flex justify-center items-center my-4">
-          <StarIcon w={8} h={8} color="blue.500" />
+          <ArrowDownIcon w={8} h={8} color="blue.500" />
         </Box>
         <Heading className="text-2xl text-gray-900 mb-1 text-center font-semibold">
-          Welcome!
-        </Heading>
-        <Heading className="text-sm text-gray-600 mb-6 text-center">
-          Sign in to your account
+          Create account!
         </Heading>
         <Box className="flex flex-col items-start">
           <VStack spacing={4} align="stretch" className="w-full mb-4">
@@ -44,15 +41,24 @@ const Signin = () => {
                 </InputRightElement>
               </InputGroup>
             </FormControl>
+            <FormControl>
+              <FormLabel className="text-black">Confirm password</FormLabel>
+              <InputGroup>
+                <Input className="text-black border-b-2 border-solid" />
+                <InputRightElement className="items-center flex h-full mx-2">
+                  <LockIcon w={4} h={4} color="gray.400" />
+                </InputRightElement>
+              </InputGroup>
+            </FormControl>
           </VStack>
           <Text className="text-black">
-            Don't have an account yet?{" "}
-            <Link color="teal.500" href="/register">
-              Sign Up
+            Already have an account?{" "}
+            <Link color="teal.500" href="/login">
+              Sign In
             </Link>
           </Text>
           <Button className="bg-blue-600 rounded-md text-white px-6 py-2 mt-4">
-            Login
+            Register
           </Button>
         </Box>
       </Box>
--- frontend/encrypted-messenger/src/App.jsx
+++ frontend/encrypted-messenger/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Signin from "../pages/Signin";
+import Signup from "../pages/Signup";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/login" element={<Signin />} />
+        <Route path="/register" element={<Signup />} />
       </Routes>
     </Router>
   );
