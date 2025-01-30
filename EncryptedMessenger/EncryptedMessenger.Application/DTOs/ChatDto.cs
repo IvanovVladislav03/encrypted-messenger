6061e5d61f1211133da1a5e42051244d7a605a69
Иванов Владислав Денисович <158275412+IvanovVladislav03@users.noreply.github.com>
2025-01-30 12:54:41 +0400

    added chat, message DTO's

---
+++ EncryptedMessenger/EncryptedMessenger.Application/DTOs/ChatDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncryptedMessenger.Application.DTOs
+{
+    public class ChatDto
+    {
+        public Guid Id { get; set; }
+        public string ChatName { get; set; } = string.Empty;
+        public bool IsGroup { get; set; }
+        public List<Guid> MemberIds { get; set; } = new();
+    }
+
+}
---
+++ EncryptedMessenger/EncryptedMessenger.Application/DTOs/MessageDto.cs
@@ -0,0 +1,17 @@
+﻿using EncryptedMessenger.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncryptedMessenger.Application.DTOs
+{
+    public class MessageDto
+    {
+        public Guid UserId { get; set; }
+        public string MessageContent { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+        public ICollection<Attachment> Attachments { get; set; } = new List<Attachment>();
+    }
+}
